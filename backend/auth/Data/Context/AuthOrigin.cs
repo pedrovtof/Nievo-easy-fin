c43e932b586ad4dfdbeee89a3710ae491fc62198
pedrovotf <pedrovotf@gmail.com>
2026-02-04 09:30:31 -0300

    Fix(auth): database context Csharp with pgsql and create check for databases

--- backend/auth/Data/Context/AuthOrigin.cs
+++ backend/auth/Data/Context/AuthOrigin.cs
@@ -10,10 +10,15 @@ namespace auth.Data.Context
     public class AuthOrigin : AuthDbContext
     {
         /// <summary>
+        /// Class for connect on main PGSQL database for AUTH
+        /// This context create connection only for ORIGIN, were we can use CRUD.
+        /// <returns>Connectio to Auth database PGSQL in main node</returns>
         /// </summary>
         
-        
-        public AuthOrigin()
+        protected override string KeyNameConnection => "auth_pgsql_origin";
+
+        public AuthOrigin(DbContextOptions<AuthOrigin> options, IConfiguration configuration) 
+            : base(options, configuration)
         {
             
         }
