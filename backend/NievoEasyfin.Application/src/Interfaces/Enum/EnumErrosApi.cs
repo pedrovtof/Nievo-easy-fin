8e9eb5d973ff35b0142940facbf645fee81a7a69
pedrovotf <pedrovotf@gmail.com>
2026-03-27 09:05:53 -0300

    Feateru(swagger): config swagger documentation.

--- backend/NievoEasyfin.Application/src/Interfaces/Enum/EnumErrosApi.cs
+++ backend/NievoEasyfin.Application/src/Interfaces/Enum/EnumErrosApi.cs
@@ -2,26 +2,34 @@ using System.ComponentModel;
 
 namespace NievoEasyfin.Application.Interfaces.Enum
 {
+    /// <summary>
+    /// Enum para armazenar os erros da API.
+    /// ENDPOINT + SERVICE + STATUS_CODE + ERROR_MESSAGE
+    /// </summary>
     public enum EnumErrosApi
     {
         /// <summary>
         /// Campo nome possuí valores invalidos.
         /// </summary>
         [Description("Campo nome possuí valores invalidos.")]
-        INVALID_NAME_INPUT,
+        POSTUSERASYNC_AUTHSERVICE_400_INVALID_NAME_INPUT,
 
         /// <summary>
         /// Campo senha possuí valores invalidos.
         /// </summary>
         [Description("Campo senha possuí valores invalidos.")]
-        INVALID_PASSWORD_INPUT,
+        POSTUSERASYNC_AUTHSERVICE_400_INVALID_PASSWORD_INPUT,
 
         /// <summary>
         /// Campo email possuí valores invalidos.
         /// </summary>
         [Description("Campo email possuí valores invalidos.")]
-        INVALID_EMAIL_INPUT
+        POSTUSERASYNC_AUTHSERVICE_400_INVALID_EMAIL_INPUT,
 
-
+        /// <summary>
+        /// Usuário já está cadastrado.
+        /// </summary>
+        [Description("Usuário já está cadastrado.")]
+        POSTUSERASYNC_AUTHSERVICE_409_USER_ALREADY_EXISTS
     }
 }
--- backend/NievoEasyfin.Auth/Program.cs
+++ backend/NievoEasyfin.Auth/Program.cs
@@ -1,12 +1,19 @@
 
+using System.Reflection;
 using NievoEasyfin.Application.Data.Context.Database;
 using NievoEasyfin.Application.Services.Auth;
+using NievoEasyfin.Application.Interfaces.Request;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFileAuth = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPathAuth = Path.Combine(AppContext.BaseDirectory, xmlFileAuth);
+    c.IncludeXmlComments(xmlPathAuth);
+});
 
 builder.Services.AddDbContext<AuthOrigin>();
 builder.Services.AddDbContext<AuthReplica>();
