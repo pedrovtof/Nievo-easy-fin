6e03d2db295ec7467afda344a7068a0e3216560a
pedrovotf <pedrovotf@gmail.com>
2026-03-29 17:29:58 -0300

    Feat(startup): add new startup and envFiles

--- backend/NievoEasyfin.Auth/Program.cs
+++ backend/NievoEasyfin.Auth/Program.cs
@@ -1,39 +1,16 @@
-
-using System.Reflection;
-using NievoEasyfin.Application.Data.Context.Database;
-using NievoEasyfin.Application.Services.Base.Users;
-using NievoEasyfin.Application.Services.Base.Authenticator;
-using NievoEasyfin.Application.Services.Auth;
-
-
-using NievoEasyfin.Application.Interfaces.Request;
+using NievoEasyfin.Auth;
 
 var builder = WebApplication.CreateBuilder(args);
+DotNetEnv.Env.TraversePath().Load();
+var startup = new Startup(builder.Configuration);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    var xmlFileAuth = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPathAuth = Path.Combine(AppContext.BaseDirectory, xmlFileAuth);
-    c.IncludeXmlComments(xmlPathAuth);
-});
-
-builder.Services.AddDbContext<AuthOrigin>();
-builder.Services.AddDbContext<AuthReplica>();
-
-builder.Services.AddScoped<AuthService>();
-builder.Services.AddScoped<AuthenticatorService>();
-builder.Services.AddScoped<UsersService>();
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.MapControllers();
-
-app.Run();
+startup.Configure(app, builder.Environment);
---
+++ backend/NievoEasyfin.Auth/Startup.cs
@@ -0,0 +1,44 @@
+using System.Reflection;
+using NievoEasyfin.Application.Data.Context.Database;
+using NievoEasyfin.Application.Services.Base.Users;
+using NievoEasyfin.Application.Services.Base.Authenticator;
+using NievoEasyfin.Application.Services.Auth;
+using NievoEasyfin.Application.Models;
+
+namespace NievoEasyfin.Auth
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+        
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            var xmlFileAuth = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPathAuth = Path.Combine(AppContext.BaseDirectory, xmlFileAuth);
+            c.IncludeXmlComments(xmlPathAuth);
+        });
+
+        services.AddDbContext<AuthOrigin>();
+        services.AddDbContext<AuthReplica>();
+        services.AddScoped<CryptoPassword>();
+        services.AddScoped<AuthService>();
+        services.AddScoped<AuthenticatorService>();
+        services.AddScoped<UsersService>();
+        }
+
+        // Use this method to configure the HTTP request pipeline.
+        public void Configure(WebApplication app, IWebHostEnvironment env) {
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
