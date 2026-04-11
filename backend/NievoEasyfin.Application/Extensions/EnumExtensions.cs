d4bb0aa0b6bc17c269396b97c7d75a5299932f71
pedrovotf <pedrovotf@gmail.com>
2026-04-11 13:35:05 -0300

    Fix: sintax c# codebase and summary

--- backend/NievoEasyfin.Application/Extensions/EnumExtensions.cs
+++ backend/NievoEasyfin.Application/Extensions/EnumExtensions.cs
@@ -3,12 +3,17 @@ using System.Reflection;
 
 namespace NievoEasyfin.Application.Extensions.Enum
 {
+    /// <summary>
+    /// Extension for Enum
+    /// </summary>
     public static class EnumExtensions
     {
         /// <summary>
         /// Returns the [Description] attribute value of an enum member.
         /// Falls back to ToString() if no description is found.
         /// </summary>
+        /// <param name="value">this object</param>
+        /// <returns>Enum description</returns>
         public static string GetDescription(this System.Enum value)
         {
             FieldInfo? field = value.GetType().GetField(value.ToString());
